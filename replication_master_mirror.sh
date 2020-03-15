db97d14414a68f13e6fd8bc578ef5c166cf0711c
Dmytro Babyk <dmytro@system-admin.work>
2020-03-15 20:09:43 +0200

    Update replication_master_mirror.sh

--- replication_master_mirror.sh
+++ replication_master_mirror.sh
@@ -18,13 +18,13 @@
 
 
 
-DB=testdb
+DB=tester
 DUMP_FILE="/tmp/$DB-export-$(date +"%Y%m%d%H%M%S").sql"
 
 USER=root
 PASS=
 
-LOCAL_HOST=localhost
+
 MASTER_HOST=10.9.96.3
 SLAVE_HOSTS=(10.9.96.4)
 
@@ -100,67 +100,5 @@ do
 done
 
 
-echo "MASTER: $SLAVE_HOST"
 
-mysql -h $SLAVE_HOST "-u$USER" "-p$PASS" $DB <<-EOSQL &
-	GRANT REPLICATION SLAVE ON *.* TO '$USER'@'%' IDENTIFIED BY '$PASS';
-	FLUSH PRIVILEGES;
-	FLUSH TABLES WITH READ LOCK;
-	DO SLEEP(3600);
-EOSQL
-
-echo "  - Waiting for database to be locked"
-sleep 3
-
-# Dump the database (to the client executing this script) while it is locked
-echo "  - Dumping database to $DUMP_FILE"
-mysqldump -h $SLAVE_HOST "-u$USER" "-p$PASS" --opt $DB > $DUMP_FILE
-echo "  - Dump complete."
-
-# Take note of the master log position at the time of dump
-MASTER_STATUS=$(mysql -h $SLAVE_HOST "-u$USER" "-p$PASS" -ANe "SHOW MASTER STATUS;" | awk '{print $1 " " $2}')
-LOG_FILE=$(echo $MASTER_STATUS | cut -f1 -d ' ')
-LOG_POS=$(echo $MASTER_STATUS | cut -f2 -d ' ')
-echo "  - Current log file is $LOG_FILE and log position is $LOG_POS"
-
-# When finished, kill the background locking command to unlock
-kill $! 2>/dev/null
-wait $! 2>/dev/null
-
-echo "  - Master database unlocked"
-
-##
-# SLAVES
-# ------
-# Import the dump into slaves and activate replication with
-# binary log file and log position obtained from master.
-##
-
-for SLAVE_HOST in "${SLAVE_HOSTS[@]}"
-do
-	echo "SLAVE: $MASTER_HOST"
-	echo "  - Creating database copy"
-	mysql -h $LOCAL_HOST  "-u$USER" "-p$PASS" -e "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB;"
-	scp $DUMP_FILE $MASTER_HOST:$DUMP_FILE >/dev/null
-	mysql -h $LOCAL_HOST  "-u$USER" "-p$PASS" $DB < $DUMP_FILE
-
-	echo "  - Setting up slave replication"
-	mysql -h $LOCAL_HOST  "-u$USER" "-p$PASS" $DB <<-EOSQL &
-		STOP SLAVE;
-		CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',
-		MASTER_USER='$USER',
-		MASTER_PASSWORD='$PASS',
-		MASTER_LOG_FILE='$LOG_FILE',
-		MASTER_LOG_POS=$LOG_POS;
-		START SLAVE;
-	EOSQL
-	# Wait for slave to get started and have the correct status
-	sleep 2
-	# Check if replication status is OK
-	SLAVE_OK=$(mysql -h $LOCAL_HOST  "-u$USER" "-p$PASS" -e "SHOW SLAVE STATUS\G;" | grep 'Waiting for master')
-	if [ -z "$SLAVE_OK" ]; then
-		echo "  - Error ! Wrong slave IO state."
-	else
-		echo "  - Slave IO state OK"
-	fi
-done
+##############END###########################################
